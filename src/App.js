39cdf8b1452f6467161058b0ec1793d6f14ad231
rdkulp <rdkulp@gmail.com>
2020-04-28 11:06:33 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { VictoryPie, VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack } from 'victory';
+
 
 function App() {
   return (
@@ -8,15 +10,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A data exploration using React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://rhombusrose.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Back to Rhombus Rose Website
         </a>
       </header>
     </div>
